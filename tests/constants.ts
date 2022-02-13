4fe158355c6cc8d40842534539e2a4d744745e36
NetWin <netwin@codedcomplexity.de>
2022-02-13 03:05:29 +0100

    feat: make free/pro path more robust

--- tests/constants.ts
+++ tests/constants.ts
@@ -1,2 +1,4 @@
-export const PRO_PATH = './fa-files/pro-all.js';
-export const FREE_PATH = './fa-files/free-all.js';
+import * as path from 'path';
+
+export const FREE_PATH = path.join(__dirname, '../fa-files/free-all.js');
+export const PRO_PATH = path.join(__dirname, '../fa-files/pro-all.js');
